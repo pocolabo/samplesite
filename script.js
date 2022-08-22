de7933a91314c0f42f453f075bd127a9d9670380
pocolabo <pocolabo@users.noreply.github.com>
2022-08-22 10:56:12 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-btn').addEventListener('click', function(){
+   document.querySelector('.nav').classList.toggle('is-active');
+   document.querySelector('.hbg__line').classList.toggle('is-active');
+});
